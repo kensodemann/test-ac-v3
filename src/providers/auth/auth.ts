43f424537ce43f00fca54e78ba5f1a3fec4a9209
Ken Sodemann <ken@ionic.io>
2021-02-16 07:50:37 -0600

    add android

--- src/providers/auth/auth.ts
+++ src/providers/auth/auth.ts
@@ -15,12 +15,12 @@ export class AuthProvider extends IonicAuth {
       logLevel: 'DEBUG',
       scope:
         'openid offline_access email profile https://dtjacdemo.onmicrosoft.com/ed8cb65d-7bb2-4107-bc36-557fb680b994/demo.read',
-      platform: platform.is('hybrid') ? 'cordova' : 'web',
-      redirectUri: platform.is('hybrid')
-        ? 'msauth://com.ionic.acprovider/O5m5Gtd2Xt8UNkW3wk7DWyKGfv8%3D'
+      platform: platform.is('cordova') ? 'cordova' : 'web',
+      redirectUri: platform.is('cordova')
+        ? 'msauth://com.ionic.acprovider/k3krTUlwC1YA3aJc0FC7wQ11QBU%3D'
         : 'http://localhost:8100',
-      logoutUrl: platform.is('hybrid')
-        ? 'msauth://com.ionic.acprovider/O5m5Gtd2Xt8UNkW3wk7DWyKGfv8%3D'
+      logoutUrl: platform.is('cordova')
+        ? 'msauth://com.ionic.acprovider/k3krTUlwC1YA3aJc0FC7wQ11QBU%3D'
         : 'http://localhost:8100'
     });
   }
